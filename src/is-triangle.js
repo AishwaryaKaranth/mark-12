b093c8151f9f28a4a0b4711f177e3392b2865910
Aishwarya Karanth <35144851+AishwaryaKaranth@users.noreply.github.com>
2022-10-15 22:48:05 +0530

    resolved comments

--- src/is-triangle.js
+++ src/is-triangle.js
@@ -8,14 +8,17 @@ var isTriangle = document.querySelector(".result");
 
 const calculateSumOfAngles = () => {
   if (
-    firstAngle.value <= 0 ||
-    secondAngle.value <= 0 ||
-    thirdAngle.value <= 0
+    parseInt(firstAngle.value) <= 0 ||
+    parseInt(secondAngle.value) <= 0 ||
+    parseInt(thirdAngle.value) <= 0
   ) {
     isTriangle.innerText =
       "Angles of the triangle cannot be less than or equal to 0!";
   } else {
-    let sumOfAngles = firstAngle.value + secondAngle.value + thirdAngle.value;
+    let sumOfAngles =
+      parseInt(firstAngle.value) +
+      parseInt(secondAngle.value) +
+      parseInt(thirdAngle.value);
     sumOfAngles === 180
       ? (isTriangle.innerText = `This is a triangle!`)
       : (isTriangle.innerText = `This is not a triangle!`);
